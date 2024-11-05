a7fbb9e198ee70035e88080028b6fa392c847b54
madara07 <madara07@itachi>
2024-11-05 06:48:56 +0530

    second commit

--- chat_room_1/chat_project/chat/views.py
+++ chat_room_1/chat_project/chat/views.py
@@ -1,8 +1,17 @@
+# from django.shortcuts import render
+# from .models import Message
+# # Create your views here.
+
+
+# def chat_room (request,room_name):
+#     message = Message.objects.filter(room_name=room_name)
+#     return render (request , 'chat_room.html',{ 'room_name':room_name,'message':message})# chat/views.py
 from django.shortcuts import render
 from .models import Message
-# Create your views here.
 
-
-def chat_room (request,room_name):
-    message = Message.objects.filter(room_name=room_name)
-    return render (request , 'chat_room.html',{ 'room_name':room_name,'message':message})
+def chat_room(request, room_name):
+    messages = Message.objects.filter(room_name=room_name)
+    return render(request, 'chat_room.html', {
+        'room_name': room_name,
+        'messages': messages
+    })
